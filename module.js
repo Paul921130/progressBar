26bd3996157c4478a45af7046a9d7ac4d1f2bd42
Unknown <paul86677889@gmail.com>
2018-01-02 17:53:42 +0800

    FristCommit

---
+++ module.js
@@ -0,0 +1,103 @@
+(function($) {
+	
+	'use strict';
+
+	var ModuleName='ps_flpk';
+
+	var Module = function ( ele, options ) {
+		this.ele = ele;
+		this.$ele = $(ele);
+		this.option = options;
+		this.satePoint =['zero','done'];
+		this.sate= 0;
+		this.timer
+	};
+	
+	// 下面是DEFAULTS物件 
+	Module.DEFAULTS = {
+			speed:3000,
+            radius: '0px',
+            height: '3px',
+            width: '100%'
+     }
+	
+
+	
+	// document.getElementById('result').style.transition=this.option.speed+'ms';
+	
+	Module.prototype.init = function () {
+ 			var progressNumber =20;
+ 			document.getElementById('result').style.width= progressNumber+'%';
+			this.addTransition();
+	};
+
+
+
+
+
+	Module.prototype.assignPercent = function(progressNumber){
+		this.addTransition();
+		var progressNumber =50;
+		document.getElementById('result').style.width= progressNumber+'%';
+		console.log(progressNumber +'%');
+		return  progressNumber;
+	}
+
+
+	Module.prototype.nextProgress = function(progressNumber){
+		this.addTransition();
+		var progressNumber =(100 - progressNumber ) * 0.2 + progressNumber ;
+		document.getElementById('result').style.width=progressNumber+'%';
+		console.log(progressNumber+'%');
+		return  progressNumber; 
+	}
+
+	Module.prototype.doneProgress = function(progressNumber){
+		this.addTransition();
+		var progressNumber= 100;
+		document.getElementById('result').style.width=progressNumber+'%';
+		console.log(progressNumber+'%');
+		return  progressNumber; 
+	}
+
+	Module.prototype.zeroProgress = function(progressNumber){
+		this.addTransition();
+		var progressNumber= 0;
+		document.getElementById('result').style.width=progressNumber+'%';
+		console.log(progressNumber+'%');
+		return  progressNumber;
+	}
+
+	Module.prototype.addTransition = function() {
+		// if ( ! this.$ele.hasClass('transition') ) {
+		// 	this.$ele.addClass('transition');
+		// };
+		document.getElementById('result').style.transition=this.option.speed+'ms';
+	};//判斷ele是否有transition,如果沒有則加。
+
+
+
+	$.fn[ModuleName] = function ( method, options ) {
+		return this.each(function(){
+			var $this = $(this);
+			var module = $this.data( ModuleName );
+			var opts = null;
+			if ( !!module ) {
+				if ( typeof method === 'string' &&  typeof options === 'undefined' ) {
+					module[method]();
+				} else if ( typeof method === 'string' && typeof options === 'object' || typeof options === 'string' ) {
+					module[method](options);
+				} else {
+					console.log('unsupported options!');
+				}
+			} else {
+				opts = $.extend( {}, Module.DEFAULTS, ( typeof method === 'object' && method ), ( typeof options === 'object' && options ) );
+				module = new Module(this, opts);
+				$this.data( ModuleName, module );
+				module.init();
+				// 執行的function	
+		}
+		});
+	};
+
+})(jQuery);
---
+++ usage.js
@@ -0,0 +1,37 @@
+
+<div class="ps_flpk">
+	<div class="result"></div>
+</div>
+
+
+$('.ps_flpk').ps_flpk({
+	// 設定動畫在多久內完成
+	speed:1000
+});
+
+$('.ps_flpk').ps_flpk('assignPercent', 50, function( progressNumber ) {
+	console.log(progressNumber);
+});
+
+/*
+規則如下(也可參照示意圖)：(100 - 已完成進度百分比) * 0.2 + 已完成的進度百分比 = 進度條要跑到的百分比
+例如：
+第1次回傳資料：(100-0)*0.2+0=20
+第2次回傳資料：(100-20)*0.2+20=36
+第3次回傳資料：(100-36)*0.2+36=48.8
+第4次以此類推...
+ */
+
+$('.ps_flpk').ps_flpk('nextProgress', function( progressNumber ) {
+	console.log(progressNumber);
+});
+
+$('.ps_flpk').ps_flpk('doneProgress', function( progressNumber ) {
+	console.log(progressNumber);
+});
+
+$('.ps_flpk').ps_flpk('zeroProgress', function( progressNumber ) {
+	console.log(progressNumber);
+});
+
+
